f8ddae3736dc1cfcec3beced974036d569ba42f4
Rahul <unniamelia@gmail.com>
2025-03-06 21:58:00 +0400

    Side bar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import NewDocumentButton from "./NewDocumentButton";
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -13,22 +14,25 @@ import {
 import { useUser } from "@clerk/nextjs";
 import { collectionGroup, query, where } from "firebase/firestore";
 import { db } from "@/firebase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RoomDocument } from "@/types/types";
+import SidebarOptions from "./SidebarOptions";
 
 const Sidebar = () => {
   const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({ owner: [], editor: [] });
   const [data, loading, error] = useCollection(
     user &&
       query(
         collectionGroup(db, "rooms"),
-        where("userId", "==", user.emailAddresses[0].toString())
+        where("userId", "==", user.emailAddresses[0].emailAddress)
       )
   );
-
   useEffect(() => {
     if (!data) return;
-
     const grouped = data.docs.reduce<{
       owner: RoomDocument[];
       editor: RoomDocument[];
@@ -50,17 +54,46 @@ const Sidebar = () => {
       },
       { owner: [], editor: [] }
     );
+    setGroupedData(grouped);
   }, [data]);
 
   const menuOptions = (
     <>
       <NewDocumentButton />
 
-      {/* My Documents */}
-      {/* List ... */}
+      <div className="flex flex-col space-y-4 md:max-w-36 py-4">
+        {/* My Documents */}
+        {groupedData.owner.length === 0 ? (
+          <h2 className="text-gray-500 font-semibold text-sm">
+            No Documents Found
+          </h2>
+        ) : (
+          <>
+            <h2 className="text-gray-500 font-semibold text-sm">
+              My Documents
+            </h2>
+            {groupedData.owner.map((doc: RoomDocument) => (
+              <SidebarOptions
+                href={`/doc/${doc.id}`}
+                id={doc.id}
+                key={doc.id}
+              />
+            ))}
+          </>
+        )}
+      </div>
 
       {/* Shared with me */}
-      {/* List ... */}
+      {groupedData.editor.length > 0 && (
+        <>
+          <h2 className="text-gray-500 font-semibold text-sm">
+            Shared with me
+          </h2>
+          {groupedData.owner.map((doc: RoomDocument) => (
+            <SidebarOptions href={`/doc/${doc.id}`} id={doc.id} key={doc.id} />
+          ))}
+        </>
+      )}
 
       {/* Trash */}
       {/* List ... */}
@@ -74,9 +107,10 @@ const Sidebar = () => {
           <SheetTrigger>
             <MenuIcon className="p-2 hover:opactity-30 rounded-lg " size={40} />
           </SheetTrigger>
-          <SheetContent side={"left"}>
+          <SheetContent side={"left"} aria-describedby="Menu bar">
             <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetTitle></SheetTitle>
+              <SheetDescription>Menu Options</SheetDescription>
               <div>{menuOptions}</div>
             </SheetHeader>
           </SheetContent>
